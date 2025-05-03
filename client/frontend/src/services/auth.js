2a1b29713402c950b4ab1a7cc90cf429c2b56a08
Dhiyanah <dheeliyaudeen@gmail.com>
2025-05-04 03:53:23 +0530

    added the delete favorites logic in the auth.js file

--- client/frontend/src/services/auth.js
+++ client/frontend/src/services/auth.js
@@ -22,30 +22,9 @@ export const getFavorites = async (token) => {
   });
 };
 
+export const removeFavorite = async (token, countryCode) => {
+  return await axios.delete(`http://localhost:5000/api/favorites/${countryCode}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
 
-
-/*export const getFavorites = async (token) => {
-  try {
-    // Fetch favorite country codes from your backend
-    const response = await axios.get("http://localhost:5000/api/favorites", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    // Now fetch the country details from the REST Countries API
-    const countryPromises = response.data.favorites.map((code) =>
-      fetch(`https://restcountries.com/v3.1/alpha/${code}`)
-        .then((response) => response.json())
-        .then((data) => data[0])
-    );
-
-    // Wait for all country details to be fetched
-    const countries = await Promise.all(countryPromises);
-
-    return countries;
-  } catch (error) {
-    console.error("Error fetching favorites or country details:", error);
-    throw error;
-  }
-};*/
