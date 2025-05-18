d2f6f744c3f1d563e3f9f9be0450eb710b925e01
Dhiyanah <dheeliyaudeen@gmail.com>
2025-05-19 02:30:49 +0530

    changed the url of the test

--- client/frontend/src/RegisterForm.test.js
+++ client/frontend/src/RegisterForm.test.js
@@ -43,7 +43,7 @@ describe("RegisterForm", () => {
     // Wait for axios.post to be called
     await waitFor(() => {
       expect(axios.post).toHaveBeenCalledTimes(1);
-      expect(axios.post).toHaveBeenCalledWith("http://localhost:5000/api/auth/register", {
+      expect(axios.post).toHaveBeenCalledWith("https://40972122-e0d5-412f-82c6-143e48c86c58-dev.e1-us-east-azure.choreoapis.dev/country-hop-backend/country-hop-backend/v1.0/api/auth/register", {
         username: "dhiyanah",
         password: "secret123",
       });
